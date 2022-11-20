417376672a7ca2c57a13093ca3570f361b3c5bc5
Sou00 <igorlonak@gmail.com>
2022-11-20 07:59:57 +0100

    Forgot about draw
    Simple counter of 9 moves

--- tictactoe.sh
+++ tictactoe.sh
@@ -4,6 +4,7 @@ LIST=("." "." "." "." "." "." "." "." ".")
 WIN=("012" "345" "678" "036" "147" "258" "048" "246")
 END=0
 USER=1
+COUNTER=0
 
 function CHECK {
 	for W in ${WIN[@]}
@@ -35,8 +36,14 @@ function DISPLAY {
 
 while [ $END = 0 ]
 do
+
+	if [ $COUNTER = 9 ]
+	then
+		echo "Draw!"
+		break
+	fi
 	echo "User$USER move:"
-	read MOVE
+	read MOVE	
 	if [[ $MOVE =~ ^[1-9]$ ]]
 	then
 		if [ ${LIST[ MOVE - 1 ]} = "." ]
@@ -44,10 +51,12 @@ do
 			if [ $USER = 1 ];
 			then
 				LIST[MOVE - 1]="X"
+				let "COUNTER += 1"
 				CHECK
 				USER=2
 			else
 				LIST[MOVE - 1]="O"
+				let "COUNTER += 1"
 				CHECK
 				USER=1
 			fi
